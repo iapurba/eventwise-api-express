a00fddeee901e1bd478567c0bc6d24817444ab71
Apurba Panja <apurbapanja1@gmail.com>
2023-08-25 22:15:13 +0530

    Fixed Auth when user is  not exists

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -9,15 +9,17 @@ export const verifyToken = async (req, res, next) => {
     }
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        const user = await User.findOne({_id: decoded._id});
+        const user = await User.findOne({ _id: decoded._id });
+        if (!user) {
+            return res.status(404).json({ error: "User Not Found" });
+        }
         req.user = user;
-        console.log("Current User: ", req.user);
         next();
     } catch (error) {
         if (error.name === "TokenExpiredError") {
             res.status(401).json({ message: "Token has expired. Please log in again." });
-          } else {
+        } else {
             res.status(500).json({ message: "Internal server error" });
-          }
+        }
     }
 };
--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,7 +1,4 @@
 export const authorizeUser = (req, res, next) => {
-    if (!req.user) {
-        return res.status(404).json({ error: "User Not Found" });
-    }
     const authenticatedUserId = req.user._id.toString();
     const requestedUserId = req.params.userId;
 
