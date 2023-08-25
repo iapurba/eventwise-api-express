e4b5db043ada9754041b55c966ad179b5df16cbd
Apurba Panja <apurbapanja1@gmail.com>
2023-08-25 21:18:59 +0530

    Implement User authentication

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/mongodb.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+// import eventRoutes from "./routes/eventRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Establish the MongoDB database connection
+connectDB();
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Express server is running on ${port}`);
+});
---
+++ src/middlewares/authentication.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const verifyToken = async (req, res, next) => {
+    const authHeader = req.header("Authorization");
+    const token = authHeader && authHeader.replace("Bearer ", "");
+    if (!token) {
+        return res.status(401).json({ error: "Access denied. Token missing." });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const user = await User.findOne({_id: decoded._id});
+        req.user = user;
+        console.log("Current User: ", req.user);
+        next();
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            res.status(401).json({ message: "Token has expired. Please log in again." });
+          } else {
+            res.status(500).json({ message: "Internal server error" });
+          }
+    }
+};
