d8a83995cd0f44e806f35d6e904d25c76074d551
Apurba Panja <apurbapanja1@gmail.com>
2023-09-02 17:25:49 +0530

    Update User schema to for additional info

--- src/models/User.js
+++ src/models/User.js
@@ -1,21 +1,57 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    role: {
+const userSchema = new Schema({
+    email: {
         type: String,
-        enum: ["user", "organizer", "admin"],
-        default: "user",
         required: true,
-    }
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    emailVerified: Boolean,
+    phoneVerified: Boolean,
+    profile: {
+        firstName: String,
+        lastName: String,
+        profilePicture: String,
+        bio: String,
+        location: String,
+    },
+    address: {
+        addressLine1: String,
+        addressLine2: String,
+        landmark: String,
+        city: String,
+        state: String,
+        pincode: String,
+    },
+    paymentMethods: [],
+    favouriteEvents: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Event',
+        },
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 userSchema.pre('save', async function (next) {
     const user = this;
-    if (!user.isModified("password")) {
+    if (!user.isModified('password')) {
         return next();
     }
     const hashedPassword = await bcrypt.hash(user.password, 10);
@@ -27,6 +63,6 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
