17b5d69937d5498f01ec8a9bbb3cce93053d66cf
Apurba Panja <apurbapanja1@gmail.com>
2023-11-19 00:00:11 +0530

    Remove all the publisher operations

--- src/controllers/publishers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as authController } from './publisherAuthController.js';
-export { default as eventController } from './publisherEventController.js';
-export { default as ticketController } from './publisherTicketController.js';
--- src/routes/publishers/publisherAuthRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { authController } from '../../controllers/publishers/index.js';
-
-const router = express.Router();
-
-router.post('/register', authController.registerPublisher);
-router.post('/login', authController.loginPublisher);
-
-export default router;
