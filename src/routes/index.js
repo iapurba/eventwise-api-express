927e4e62623de0cda31da7784af1b7d32a8fd4be
Apurba Panja <apurbapanja1@gmail.com>
2023-11-19 20:46:08 +0530

    Remove publisher related endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,3 @@ export { default as authRoutes } from './authRoutes.js';
 export { default as userRoutes } from './userRoutes.js';
 export { default as eventRoutes } from './eventRoutes.js';
 export { default as bookingRoutes } from './bookingRoutes.js';
-
-// Publisher specific routes
-export { default as publisherEventRoutes } from './publishers/publisherEventRoutes.js';
-export { default as publisherAuthRoutes } from './publishers/publisherAuthRoutes.js'
